d3d9148760fbcdbc77c521249ca4425112ac6337
Stormy <74722466+loniop@users.noreply.github.com>
2023-08-16 14:15:00 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,14 @@
-function dark() {
-  bew();
-  reload();
-};
-function bew() {
-         let model-viewerDiv = document.getElementById("img");
-         model-viewerDiv.environment-image = 'ot.hdr'; // replace the image on button click.
-};
-function reload(){
-    var container = document.getElementById("img");
-    var content = container.innerHTML;
-    container.innerHTML= content; 
-    
-   //this line is to watch the result in console , you can remove it later	
-    console.log("Refreshed"); 
-}
+var space = false;
+var dark = document.querySelector("model-viewer"); 
+$(function() {
+  $(document).keyup(function(evt) {
+    if (evt.keyCode == 32) {
+      space = false;
+    }
+  }).keydown(function(evt) {
+    if (evt.keyCode == 32) {
+      space = true;
+      input.toggleAttribute("environment-image");    
+    }
+  });
+});
